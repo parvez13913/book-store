405fc4f710bd7fb38539ccb599a13c8e2e678284
parvez13913 <parvezz13913@gmail.com>
2023-08-16 19:18:54 +0600

    [DONE] modify apiSlice

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://book-store-backend-peach.vercel.app/api/v1/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("accessToken");
       if (token) {
